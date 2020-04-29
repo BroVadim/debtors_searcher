c5ba3d74509560a9ea4ffb665a2f9ac51cfd3db7
BroVadim <BroVadim1996@yandex.ru>
2020-04-29 16:47:37 +0300

    Добавил необходимые скрипты и файлы для работы

---
+++ create_debtor_table.sql
@@ -0,0 +1,17 @@
+CREATE TABLE debtorInfo(
+debtor_id			INT,
+debtor_category		NVARCHAR(50)	NOT NULL,
+debtor_inn			INT,
+debtor_ogrnip		INT,
+debtor_snils		INT,
+debtor_region		NVARCHAR(MAX),
+debtor_address		NVARCHAR(MAX),
+debtor_link			NVARCHAR(MAX)
+);
+
+CREATE TABLE debtor(
+debtor_id INT IDENTITY(1,1),
+debtor_firstname NVARCHAR(MAX),
+debtor_middlename NVARCHAR(MAX),
+debtor_lastname	NVARCHAR(MAX)
+);
